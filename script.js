c30a58acc106e493afafd1b6e93fbd2f6868ce56
Jakob Rietzler <jakob.rietzler@gmail.com>
2026-05-26 01:04:04 +0200

    comment

--- script.js
+++ script.js
@@ -281,10 +281,10 @@ function loadProject(projectData){
 
 downloadButton.addEventListener('click', downloadAll);
 
+// check for cmd s
 document.addEventListener('keydown', (e) => {
     if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
         e.preventDefault();
-
         downloadAll();
     }
 });
