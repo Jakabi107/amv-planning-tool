ad80af8ae539be07e9b4d766538479e7aaa114af
Jakob Rietzler <jakob.rietzler@gmail.com>
2026-05-29 01:06:22 +0200

    add formating script

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    // Simulates a browser environment inside Node.js
-    environment: 'happy-dom', 
-    
-    // Optional: Makes test functions like 'describe', 'test', and 'expect' 
-    // globally available so you don't have to import them in every file.
-    globals: true, 
-  },
-})
+	test: {
+		// Simulates a browser environment inside Node.js
+		environment: "happy-dom",
+
+		// Optional: Makes test functions like 'describe', 'test', and 'expect'
+		// globally available so you don't have to import them in every file.
+		globals: true,
+	},
+});
